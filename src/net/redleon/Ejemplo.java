d7623bdec95f279c0702256a6d582516259d057a
Leon Felipe Villavicencio Fernandez <leon@redleon.net>
2016-09-02 10:29:05 -0500

    Cambios del segundo commit

--- src/net/redleon/Ejemplo.java
+++ src/net/redleon/Ejemplo.java
@@ -3,7 +3,9 @@ package net.redleon;
 public class Ejemplo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!");
+		System.out.println("Hola mundo!!!!!!!");
+		System.out.println("Hola que tal!");
+		System.out.println("Hola a todos!");
 
 	}
 
